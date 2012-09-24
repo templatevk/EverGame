4a6ac5967f55878977e3b8e692da2355fc18a7e2
user <user@edu6.kvazar-micro.zp.ua>
2012-09-24 16:23:47 +0300

    m

--- src/pp/game/activities/MainMenuActivity.java
+++ src/pp/game/activities/MainMenuActivity.java
@@ -3,6 +3,7 @@ package pp.game.activities;
 import pp.game.*;
 import android.app.*;
 import android.os.*;
+import android.view.*;
 
 public class MainMenuActivity extends Activity {
 	public MainMenuActivity() {
@@ -12,18 +13,10 @@ public class MainMenuActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_menu_activity);		
-		
-		
-//		// Setting gradient foreground for main menu items
-//		Shader foreground = 	
-//		
-//		TextView view;
-//		view = (TextView)findViewById(R.id.exitBtn);
-//		view.getPaint().setShader(foreground);
-//		view = (TextView)findViewById(R.id.highScoresBtn);
-//		view.getPaint().setShader(foreground);
-//		view = (TextView)findViewById(R.id.newGameBtn);
-//		view.getPaint().setShader(foreground);
+		setContentView(R.layout.main_menu_activity);
+	}
+	
+	public void onNewGameClicked(View view) {
+		System.out.println("Hello");
 	}
 }
